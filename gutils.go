5fcdebe5e74fc488347d383067ddcaaeaa9c9f9f
Senthil <inbioz@gmail.com>
2021-10-29 07:18:55 +0100

    func to create folder

---
+++ gutils.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"os"
+
+	"log"
+)
+
+// CreateFolder creates a folder
+func CreateFolder(folder string, forceCreate int) error {
+
+	folderInfo, err := os.Stat(folder)
+	if os.IsNotExist(err) {
+		log.Printf("%s\n", err)
+	} else {
+		log.Printf("%s already exists\n", folderInfo.Name())
+		if forceCreate == 1 {
+			log.Printf("Deleting %s\n", folder)
+			os.RemoveAll(folder)
+		} else {
+			return nil
+		}
+	}
+
+	log.Printf("Creating = %+v\n", folder)
+	errDir := os.MkdirAll(folder, 0755)
+
+	if errDir != nil {
+		log.Println(errDir)
+	} else {
+		log.Printf("created = %+v\n", folder)
+	}
+	return errDir
+}
